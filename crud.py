2930ff033e1a012c7c701ec91a6929d0d0d0fae0
Tientien Hsieh <tthsieh0304@gmail.com>
2020-09-10 03:14:51 +0000

    Part 3 completed

--- crud.py
+++ crud.py
@@ -11,6 +11,18 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
 
 
 def create_movie(title, overview, release_date, poster_path):
@@ -26,8 +38,15 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    """Return all movies."""
 
+    return Movie.query.all()
 
+def get_movie_by_id(movie_id):
+    """Return Movie by id"""
+
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
 
